44da157daa8157350b6a254a70708b00df1f380f
cafecomdeploy <lorena.cunha@hotmail.com>
2024-09-27 08:08:24 -0300

    Alterando Schema

--- app/schemas/address_schema.py
+++ app/schemas/address_schema.py
@@ -1,12 +1,6 @@
 from pydantic import BaseModel, constr
 
 class AddressBase(BaseModel):
-    logradouro: str
-    cidade: str
-    estado: str
-    cep: str
-
-class AddressBaseUp(BaseModel):
     id: int
     logradouro: str
     cidade: str
@@ -14,10 +8,11 @@ class AddressBaseUp(BaseModel):
     cep: str
 class AddressCreate(AddressBase):
     usuario_id: int
-class AddressUpdate(AddressBaseUp):
+class AddressUpdate(BaseModel):
+    id: int
     logradouro: str
     cidade: str
     estado: str
-    cep: constr(min_length=8, max_length=10)  # validação de CEP
+    cep: str
     class Config:
         from_attributes = True
